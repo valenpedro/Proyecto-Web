7b61c319b39647b31a56c365391d330b100c8176
Valenpedro <valentina_pedroza@javeriana.edu.co>
2024-08-15 10:14:05 -0500

    Changes to be committed: 	modified:   demo/src/main/java/com/example/demo/repository/PetRepository.java 	modified:   demo/src/main/resources/static/css/pet-details.css

--- demo/src/main/java/com/example/demo/repository/PetRepository.java
+++ demo/src/main/java/com/example/demo/repository/PetRepository.java
@@ -28,27 +28,28 @@ public class PetRepository {
         Random random = new Random();
         String[] names = {"Max", "Bella", "Charlie", "Lucy", "Milo", "Luna"};
         String[] breeds = {"Golden Retriever", "Bulldog", "Poodle", "Beagle", "Shih Tzu"};
-        String[] illnesses = {"Healthy", "Injured", "Sick", "Under Observation"};
-        String[] statuses = {"Active", "Inactive"};
+        String[] illnesses = {"Saludable", "Herido", "Enfermo", "En Observación"};
+        String[] statuses = {"Activo", "Inactivo"};
         String[] photoUrls = {
-                "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*",
-                "https://www.hartz.com/wp-content/uploads/2022/04/small-dog-owners-1.jpg",
-                "https://static.vecteezy.com/system/resources/thumbnails/005/857/332/small_2x/funny-portrait-of-cute-corgi-dog-outdoors-free-photo.jpg"
+            "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*",
+            "https://www.hartz.com/wp-content/uploads/2022/04/small-dog-owners-1.jpg",
+            "https://static.vecteezy.com/system/resources/thumbnails/005/857/332/small_2x/funny-portrait-of-cute-corgi-dog-outdoors-free-photo.jpg"
         };
-
+    
         for (int i = 1; i <= count; i++) {
             pets.add(new Pet(
                     i,
                     names[random.nextInt(names.length)],
                     breeds[random.nextInt(breeds.length)],
-                    random.nextInt(15), // Age between 0 and 14
-                    random.nextFloat() * 30, // Weight between 0 and 30 kg
+                    random.nextInt(15), // Edad entre 0 y 14
+                    random.nextInt(31), // Peso entre 0 y 30 kg, ahora es un número entero
                     illnesses[random.nextInt(illnesses.length)],
-                    photoUrls[random.nextInt(photoUrls.length)], // Randomly select one of the provided URLs
+                    photoUrls[random.nextInt(photoUrls.length)], // Selección aleatoria de una de las URLs proporcionadas
                     statuses[random.nextInt(statuses.length)]
             ));
         }
-
+    
         return pets;
     }
+    
 }
