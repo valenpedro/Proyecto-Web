22cb710b771af720167727106e2959d12b46f6cf
inatalia.avila <inatalia.avila@javeriana.edu.co>
2024-09-04 11:28:46 -0500

    actualizado pet form

--- demo/src/main/resources/static/JavaScript/sidebar.js
+++ demo/src/main/resources/static/JavaScript/sidebar.js
@@ -1,17 +1,6 @@
-/* Created by Tivotal */
+const sidebar = document.querySelector('.sidebar');
+const toggleBtn = document.querySelector('.home-content i');
 
-let btn = document.querySelector(".fa-bars");
-let sidebar = document.querySelector(".sidebar");
-
-btn.addEventListener("click", () => {
-  sidebar.classList.toggle("close");
+toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('close');
 });
-
-let arrows = document.querySelectorAll(".arrow");
-for (var i = 0; i < arrows.length; i++) {
-  arrows[i].addEventListener("click", (e) => {
-    let arrowParent = e.target.parentElement.parentElement;
-
-    arrowParent.classList.toggle("show");
-  });
-}
